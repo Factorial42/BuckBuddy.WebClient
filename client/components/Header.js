dc8173ffc56bc2bfd985a897ce0ce16ba7237ea9
Mandarin Drummond <mandarind@gmail.com>
2016-04-17 16:30:14 -0400

    remove extra stuff frm header

--- client/components/Header.js
+++ client/components/Header.js
@@ -19,25 +19,9 @@ let Header = React.createClass({
 
       <Navbar>
        <Navbar.Header>
-         {/*<Link to="/">
-           <Navbar.Brand>
-              <img src="/static/img/buck.png" />
-           </Navbar.Brand>
-         </Link>*/}
          <Navbar.Toggle />
        </Navbar.Header>
        <Navbar.Collapse>
-         <Nav>
-           {/*<NavItem eventKey={1} href="#">Link</NavItem>
-           <NavItem eventKey={2} href="#">Link</NavItem>*/}
-           {/*<NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-             <MenuItem eventKey={3.1}>Action</MenuItem>
-             <MenuItem eventKey={3.2}>Another action</MenuItem>
-             <MenuItem eventKey={3.3}>Something else here</MenuItem>
-             <MenuItem divider />
-             <MenuItem eventKey={3.3}>Separated link</MenuItem>
-           </NavDropdown>*/}
-         </Nav>
          <Nav pullRight>
            {logoutNode}
          </Nav>
